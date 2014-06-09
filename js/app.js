cab5b42f21ddc818387474da16be2cb5d4f115cc
inaki <iiaranzadi@gmail.com>
2014-06-09 02:28:03 -0400

    updated the steps in ellipse and rectangle

--- js/app.js
+++ js/app.js
@@ -34,14 +34,14 @@ function TabsCtrl($scope) {
     },
     rectangle: {
       title: 'Drawing a beautiful Rectangle',
-      content: 'We are learning here how to draw a circle. This is a simple shape.  ',
+      content: 'We are learning here how to draw a rectangle. This is a simple shape.  ',
       steps: [
         'First you have to make sure that you have the libraries you need, those are jquery and twe.js.',
         'Then you create a div with some id, in this case I called it circle.',
-        'Then you create a variable in javascript referencing the div#id circle. ',
+        'Then you create a variable in javascript referencing the div#id rectangle. ',
         'Now you can create a variable with the parameters values.',
-        'And instantiate a Two object whith the params values and appending it to the div#id cicle',
-        'Well done. So now create a variable calling the method makeCircle where you put (x-position, y-position, and the ratio of the circle)',
+        'And instantiate a Two object whith the params values and appending it to the div#id rectangle',
+        'Well done. So now create a variable calling the method makeCircle where you put (x-position, y-position, width and height) of the rectangle',
         'Having done that, play a little bit filling it with a color, and i say noStroke()',
         'The last thing but very important is tu call two.update(). So when the document is ready it will render the drawing.'
       ],
@@ -49,14 +49,14 @@ function TabsCtrl($scope) {
     },
     ellipse: {
       title: 'Drawing a beautiful Ellipse',
-      content: 'We are learning here how to draw a circle. This is a simple shape.  ',
+      content: 'We are learning here how to draw a ellipse. This is a simple shape.  ',
       steps: [
         'First you have to make sure that you have the libraries you need, those are jquery and twe.js.',
-        'Then you create a div with some id, in this case I called it circle.',
-        'Then you create a variable in javascript referencing the div#id circle. ',
+        'Then you create a div with some id, in this case I called it ellipse.',
+        'Then you create a variable in javascript referencing the div#id ellipse. ',
         'Now you can create a variable with the parameters values.',
         'And instantiate a Two object whith the params values and appending it to the div#id cicle',
-        'Well done. So now create a variable calling the method makeCircle where you put (x-position, y-position, and the ratio of the circle)',
+        'Well done. So now create a variable calling the method makeCircle where you put (x-position, y-position, width, height) of the ellipse',
         'Having done that, play a little bit filling it with a color, and i say noStroke()',
         'The last thing but very important is tu call two.update(). So when the document is ready it will render the drawing.'
       ],

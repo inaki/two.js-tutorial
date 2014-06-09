9bff2eea2f2aa1748ec3b75fa4b7604c920095c6
inaki <iiaranzadi@gmail.com>
2014-06-09 02:04:57 -0400

    adding the gist code for ellipse and rectangle

--- js/figures.js
+++ js/figures.js
@@ -1,13 +1,33 @@
+// drawing a circle
 var circ = document.getElementById('circle');
-
-
-var params = { width: 200, height: 150};
-var two = new Two(params).appendTo(circ);
-
-var circle = two.makeCircle(150, 70, 50);
+var params = { width: 300, height: 150};
+var twoCirc = new Two(params).appendTo(circ);
+var circle = twoCirc.makeCircle(150, 70, 50);
 
 circle.fill = "#881111";
 circle.noStroke();
 
+twoCirc.update();
 
-two.update();
+
+// drawing a rectangle
+var rect = document.getElementById('rectangle');
+var twoRect = new Two(params).appendTo(rect);
+var rectangle = twoRect.makeRectangle(150, 0, 250, 200);
+
+rectangle.fill = "#881111";
+rectangle.noStroke();
+
+twoRect.update();
+
+
+// drawing an ellipse
+var ellip = document.getElementById('ellipse');
+var twoEllip = new Two(params).appendTo(ellip);
+var ellipse = two.makeEllipse(100, 50, 20, 80);
+
+ellipse.fill = "#881111";
+ellipse.noStroke();
+
+
+twoEllip.update();
